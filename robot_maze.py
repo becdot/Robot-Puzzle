2b30110f572837b146708d29d7f4de2d12052f2a
Becca Liss <becca.liss@gmail.com>
2012-04-14 11:39:59 -0400

    Initial string into board:
    This is the code to turn the provided string into a board with (x, y, z) coordinates, where x is the x-coordinate, y is the y-coordinate, and z is the type of space (open(.), trap(x), or goal(o))

---
+++ robot_maze.py
@@ -0,0 +1,19 @@
+def robot_puzzle(string, min_moves, max_moves, width, height):
+    board_list = list(string)
+    # adds goal spaces to the right side of the board
+    for elem in range(0, height):
+        board_list.insert(((width * elem) + (width + elem)), 'o')
+    # adds goal spaces to the bottom of the board
+    for elem in range (0, width + 1):
+        board_list.append('o')
+    print board_list
+    # turns the original string into tuples of (x-coord, y-coord, type of space (., x, or o)
+    board = []
+
+    for index, elem in enumerate(board_list):
+        x = index % (width + 1) # x coordinate
+        y = index /(width + 1) # y coordinate
+        z = elem # actual element (. or x or o)
+        board.append((x, y, z))
+    for tuple in board:
+        print tuple
